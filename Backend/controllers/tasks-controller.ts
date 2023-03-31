a6b9edeafb99c0c15333d6b0c3f115e0017838a7
Andrii Babushko <andriibabushko@gmail.com>
2023-03-31 10:41:25 +0300

    Make general improvements to controller files(jwt, bcrypt, etc)

---
+++ Backend/controllers/tasks-controller.ts
@@ -0,0 +1,255 @@
+import validator from "validator";
+import { NextFunction, Request, Response } from "express";
+import { startSession } from "mongoose";
+
+import HttpError from "../models/http-error-model";
+import { mongooseModel as TaskModel } from "../models/task-model";
+import { mongooseModel as UserModel } from "../models/user-model";
+
+const getTaskById = async (req: Request, res: Response, next: NextFunction) => {
+  const taskID: string = req.params.taskID;
+
+  let task;
+
+  try {
+    task = await TaskModel.findById(taskID);
+  } catch (e) {
+    return next(
+      new HttpError(
+        "Something went wrong while searching for some task by task ID.",
+        500
+      )
+    );
+  }
+
+  if (!task) {
+    return next(
+      new HttpError("Couldn't find a task for the provided task ID!", 404)
+    );
+  }
+
+  return res.json({ task: task.toObject({ getters: true }) });
+};
+
+const getTasksByCreatorId = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const creatorID: string = req.params.creatorID;
+  let tasks;
+
+  try {
+    tasks = await TaskModel.find({ creator: creatorID });
+  } catch (error) {
+    return next(
+      new HttpError(
+        "Something went wrong while searching for some tasks by creator ID.",
+        500
+      )
+    );
+  }
+
+  if (!tasks || tasks.length == 0) {
+    return next(
+      new HttpError("Couldn't find tasks for the provided user ID!", 422)
+    );
+  }
+
+  return res.json({ tasks: tasks.map((t) => t.toObject({ getters: true })) });
+};
+
+const getTasks = async (req: Request, res: Response, next: NextFunction) => {
+  let tasks;
+
+  try {
+    tasks = await TaskModel.find();
+  } catch (e) {
+    return next(
+      new HttpError(
+        "Something went wrong while fetching tasks data from DB.",
+        500
+      )
+    );
+  }
+
+  return res.json({ tasks: tasks.map((t) => t.toObject({ getters: true })) });
+};
+
+const createTask = async (req: Request, res: Response, next: NextFunction) => {
+  const {
+    title,
+    description,
+    deadlineDate,
+    creator,
+    tags,
+  }: {
+    title: string;
+    description: string;
+    deadlineDate: string;
+    creator: string;
+    tags: string[];
+  } = req.body;
+
+  try {
+    if (validator.isEmpty(title))
+      return next(new HttpError("Task title is empty!", 422));
+
+    if (validator.isEmpty(description))
+      return next(new HttpError("Task description is empty!", 422));
+
+    if (
+      !validator.isDate(deadlineDate) ||
+      validator.isEmpty(deadlineDate) ||
+      new Date(deadlineDate).getTime() <= new Date().getTime()
+    )
+      return next(new HttpError("Task deadline date is invalid!", 422));
+  } catch (e) {
+    return next(
+      new HttpError("Something went wrong while checking task data.", 500)
+    );
+  }
+
+  const createdTask = new TaskModel({
+    title,
+    description,
+    creationDate: new Date(),
+    deadlineDate: new Date(deadlineDate),
+    creator,
+    tags,
+  });
+  let user;
+
+  try {
+    user = await UserModel.findById(creator);
+  } catch (e) {
+    return next(
+      new HttpError(
+        "Couldn't find user for provided id! Please, check credentials.",
+        500
+      )
+    );
+  }
+
+  if(!user)
+    return next(new HttpError("Couldn't find user for provided id.", 404))
+
+  try {
+    const session = await startSession();
+    session.startTransaction();
+    await createdTask.save({ session });
+    user.tasks.push(createdTask);
+    await user.save( { session });
+    await session.commitTransaction();
+  } catch (e) {
+    return next(
+      new HttpError(
+        "Creating task failed. Pls check credentials and try again.",
+        500
+      )
+    );
+  }
+
+  return res
+    .status(201)
+    .json({ task: createdTask.toObject({ getters: true }) });
+};
+
+const updateTaskByID = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { title, description }: { title: string; description: string } =
+    req.body;
+  let task;
+  const taskID = req.params.taskID;
+
+  if (validator.isEmpty(title))
+    return next(new HttpError("Task title is empty!", 422));
+
+  if (validator.isEmpty(description))
+    return next(new HttpError("Task description is empty!", 422));
+
+  try {
+    task = await TaskModel.findById(taskID);
+  } catch (e) {
+    return next(
+      new HttpError(
+        "Something went wrong while searching for some task by task ID.",
+        500
+      )
+    );
+  }
+
+  if (!task) {
+    return next(
+      new HttpError("Couldn't find a task for the provided task ID!", 404)
+    );
+  }
+
+  task.title = title;
+  task.description = description;
+
+  try {
+    await task.save();
+  } catch (e) {
+    return next(
+      new HttpError("Something went wrong while updating task.", 500)
+    );
+  }
+
+  return res.status(200).json({ task: task.toObject({ getters: true }) });
+};
+
+const deleteTaskByID = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let task;
+  const taskID = req.params.taskID;
+
+  try {
+    task = await TaskModel.findById(taskID).populate("creator");
+  } catch (e) {
+    return next(
+      new HttpError(
+        "Something went wrong while searching for some task by task ID.",
+        500
+      )
+    );
+  }
+
+  if (!task) {
+    return next(
+      new HttpError("Couldn't find a task for the provided task ID!", 404)
+    );
+  }
+
+  try {
+    const session = await startSession();
+    session.startTransaction();
+    await task.deleteOne({ session });
+    task.creator.tasks.pull(task);
+    await task.creator.save({ session });
+    await session.commitTransaction();
+  } catch (e) {
+    return next(
+      new HttpError("Something went wrong while deleting task.", 500)
+    );
+  }
+
+  return res
+    .status(200)
+    .json({ message: `Successful deleted task with ${taskID} ID.` });
+};
+
+export {
+  getTaskById,
+  getTasksByCreatorId,
+  getTasks,
+  createTask,
+  updateTaskByID,
+  deleteTaskByID,
+};
---
+++ Backend/controllers/users-controller.ts
@@ -0,0 +1,183 @@
+import validator from "validator";
+import { NextFunction, Request, Response } from "express";
+import * as bcrypt from "bcrypt";
+import * as jwt from "jsonwebtoken";
+
+import HttpError from "../models/http-error-model";
+import { mongooseModel as UserModel } from "../models/user-model";
+
+const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
+  let users;
+
+  try {
+    users = await UserModel.find({}, "-password");
+  } catch (e) {
+    return next(
+      new HttpError(
+        "Something went wrong while fetching users data from DB.",
+        500
+      )
+    );
+  }
+
+  return res.json({
+    users: users.map((u) => u.toObject({ getters: true })),
+    success: true,
+  });
+};
+
+const signup = async (req: Request, res: Response, next: NextFunction) => {
+  const {
+    username,
+    email,
+    password,
+  }: { username: string; email: string; password: string } = req.body;
+  let indentifiedUser;
+
+  try {
+    indentifiedUser = await UserModel.findOne({ email: email });
+  } catch (e) {
+    return next(
+      new HttpError(
+        "Something went wrong while singing up! Please, try again.",
+        500
+      )
+    );
+  }
+
+  if (indentifiedUser) {
+    return next(
+      new HttpError("User already exists! Please, login instead.", 422)
+    );
+  }
+
+  if (!validator.normalizeEmail(email) && !validator.isEmail(email))
+    return next(new HttpError("Email seem to be wrong.", 422));
+
+  if (!validator.isLength(username, { min: 2 }))
+    return next(new HttpError("Username has less than 2 symbols.", 422));
+
+  if (validator.isEmpty(password))
+    return next(new HttpError("Password is empty.", 422));
+
+  let cryptPassword;
+  try {
+    cryptPassword = await bcrypt.hash(password, 12);
+  } catch (e) {
+    return next(
+      new HttpError("Couldn't hash password. Please try again.", 500)
+    );
+  }
+
+  const createdUser = new UserModel({
+    username,
+    email,
+    image:
+      "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png",
+    password: cryptPassword,
+  });
+
+  console.log(createdUser.toObject());
+
+  try {
+    await createdUser.save();
+  } catch (e) {
+    return next(
+      new HttpError(
+        "Signing up failed. Please check credentials and try again.",
+        500
+      )
+    );
+  }
+
+  console.log(createdUser);
+
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: createdUser.id, email: createdUser.email },
+      "task_management_tool_token",
+      { expiresIn: "1h" }
+    );
+  } catch (e) {
+    return next(new HttpError("Signing up failed! Please try again.", 500));
+  }
+
+  return res.status(201).json({
+    userId: createdUser.id,
+    email: createdUser.email,
+    token,
+    message: "Successfully signed up!",
+    success: true,
+  });
+};
+
+const login = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password }: { email: string; password: string } = req.body;
+  let indentifiedUser;
+
+  if (!validator.normalizeEmail(email) && !validator.isEmail(email))
+    return next(new HttpError("Entered email seem to be wrong.", 422));
+
+  if (validator.isEmpty(password))
+    return next(new HttpError("Password is empty.", 422));
+
+  try {
+    indentifiedUser = await UserModel.findOne({ email: email });
+  } catch (e) {
+    return next(
+      new HttpError(
+        "Something went wrong while logging up! Please, try again.",
+        500
+      )
+    );
+  }
+
+  if (!indentifiedUser) {
+    return next(
+      new HttpError(
+        "Can't find such user! Please, check your credentials.",
+        401
+      )
+    );
+  }
+
+  let isValidPassword: boolean = false;
+  try {
+    isValidPassword = await bcrypt.compare(password, indentifiedUser.password);
+  } catch (e) {
+    return next(
+      new HttpError(
+        "Couldn't log you in! Please, check your credentials and try again.",
+        500
+      )
+    );
+  }
+
+  if (!isValidPassword) {
+    return next(
+      new HttpError("Password is incorrect! Please, try again.", 401)
+    );
+  }
+
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: indentifiedUser.id, email: indentifiedUser.email },
+      "task_management_tool_token",
+      { expiresIn: "1h" }
+    );
+  } catch (e) {
+    return next(new HttpError("Logging in failed! Please try again.", 500));
+  }
+
+  res.json({
+    userId: indentifiedUser.id,
+    email: indentifiedUser.email,
+    token,
+    message: "Successfully logged in!",
+    success: true,
+  });
+};
+
+export { getAllUsers, login, signup };
