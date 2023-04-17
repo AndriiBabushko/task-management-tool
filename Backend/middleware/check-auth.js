c05eeca0e04658f937a4c8a81d0d858241c0db1c
Andrii Babushko <andriibabushko@gmail.com>
2023-04-17 09:41:25 +0300

    Modify check-auth middleware to save data correctly

--- Backend/middleware/check-auth.js
+++ Backend/middleware/check-auth.js
@@ -7,15 +7,17 @@ var checkJWT = function (req, res, next) {
     var token;
     var userId;
     try {
-        token = req.headers.authorization; // Authorization: 'TOKEN'
+        token = req.headers.authorization.split(" ")[1]; // Authorization: 'Bearer TOKEN'
         if (!token)
             return next(new http_error_model_1.default("Authentication failed!", 401));
         userId = jwt.verify(token, "task_management_tool_token");
-        req.userData = { userId: userId };
+        req.userData = userId;
+        req.token = token;
     }
     catch (e) {
+        console.log(e.message);
         return next(new http_error_model_1.default("Authentication failed!", 401));
     }
-    return next();
+    next();
 };
 exports.checkJWT = checkJWT;
--- Backend/middleware/check-auth.ts
+++ Backend/middleware/check-auth.ts
@@ -3,28 +3,28 @@ import * as jwt from "jsonwebtoken";
 
 import HttpError from "../models/http-error-model";
 import { IUserDataRequest } from "../ts/interfaces/IUserDataRequest";
-import { IUserJwtPayload } from "../ts/interfaces/IUserJwtPayload";
 
 export const checkJWT = (
   req: IUserDataRequest,
   res: Response,
   next: NextFunction
-) => {
+): void => {
   let token: string;
   let userId;
 
   try {
-    token = req.headers.authorization; // Authorization: 'TOKEN'
+    token = req.headers.authorization.split(" ")[1]; // Authorization: 'Bearer TOKEN'
 
-    if (!token)
-      return next(new HttpError("Authentication failed!", 401));
+    if (!token) return next(new HttpError("Authentication failed!", 401));
 
-    userId = jwt.verify(token, "task_management_tool_token") as IUserJwtPayload;
+    userId = jwt.verify(token, "task_management_tool_token");
 
-    req.userData = { userId };
+    req.userData = userId;
+    req.token = token;
   } catch (e) {
+    console.log(e.message);
     return next(new HttpError("Authentication failed!", 401));
   }
 
-  return next();
+  next();
 };
