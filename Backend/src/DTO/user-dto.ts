67c3ff785544c2a385f2bd81b5877d918a8e737b
Andrii Babushko <andriibabushko@gmail.com>
2023-05-25 10:02:50 +0300

    Changes to user DTO

--- Backend/src/DTO/user-dto.ts
+++ Backend/src/DTO/user-dto.ts
@@ -1,13 +1,27 @@
 export default class UserDto {
-    email;
-    password;
-    id;
-    isActivated;
+  id;
+  name;
+  surname;
+  username;
+  email;
+  password;
+  image;
+  tasks;
+  roles;
+  groups;
+  isActivated;
 
-    constructor(model) {
-        this.email = model.email;
-        this.password = model.password;
-        this.id = model._id;
-        this.isActivated = model.isActivated;
-    }
+  constructor(model) {
+    this.id = model._id;
+    this.name = model.name;
+    this.surname = model.surname;
+    this.username = model.username;
+    this.email = model.email;
+    this.password = model.password;
+    this.image = model.image;
+    this.tasks = model.tasks;
+    this.roles = model.roles;
+    this.groups = model.groups;
+    this.isActivated = model.isActivated;
+  }
 }
