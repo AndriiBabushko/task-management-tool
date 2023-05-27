120580b1109ac492eab628ac3eaef0c27364070f
Andrii Babushko <andriibabushko@gmail.com>
2023-05-27 19:21:02 +0300

    Changes to group task and user models

--- Backend/src/models/task-model.ts
+++ Backend/src/models/task-model.ts
@@ -12,8 +12,9 @@ const taskSchema = new Schema<ITask>({
   deadlineDate: { type: Date, required: false, default: null, index: { expires: '0s' } },
   creator: { type: Types.ObjectId, required: true, ref: 'User' },
   tags: [{ type: Types.ObjectId, required: false, ref: 'Tag' }],
-  category: { type: Types.ObjectId, required: false, ref: 'Category' },
+  categories: [{ type: Types.ObjectId, required: false, ref: 'Category' }],
   access: { type: String, required: true, default: 'private' },
+  isCompleted: { type: Boolean, required: true, default: false },
 });
 
 const mongooseModel = model<ITask>('Task', taskSchema);
