a33c610c9990a8fa3b52ea44596b296110c74ff0
Andrii Babushko <andriibabushko@gmail.com>
2023-06-07 15:08:31 +0300

    add axios util

---
+++ client/src/app/utils/axios.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { AuthResponse } from '../models/response/AuthResponse';
+import AuthService from '../services/AuthService';
+
+export const API_URL = 'http://localhost:5000/api';
+
+const axiosInstance = axios.create({ baseURL: API_URL, withCredentials: true });
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem(`token`);
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+axiosInstance.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status == 401) {
+      try {
+        const response = await AuthService.checkAuth();
+        localStorage.setItem('token', response.data.accessToken);
+        return axiosInstance.request(originalRequest);
+      } catch (e) {
+        throw e;
+      }
+    }
+  },
+);
+
+export { axiosInstance };
