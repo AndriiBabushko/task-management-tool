93bf5e1aa3d75a43b5dc4feb75262407f0395e5d
Andrii Babushko <andriibabushko@gmail.com>
2023-06-07 15:09:32 +0300

    add tasks pages

---
+++ client/src/pages/tasks/CreateTaskPage.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from 'react';
+
+export const CreateTaskPage: FC = () => {
+  return <></>;
+};
---
+++ client/src/pages/tasks/TaskPage.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from 'react';
+
+export const TaskPage: FC = () => {
+  return <h1>Task page</h1>;
+};
