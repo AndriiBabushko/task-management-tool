49fe6f1ed625a70417a8b9299907b91567477599
Andrii Babushko <andriibabushko@gmail.com>
2023-06-10 18:56:40 +0300

    update index.tsx file

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,18 +6,15 @@ import './index.css';
 import { App } from './app/App';
 import { RootStoreContext } from './app/context/rootStoreContext';
 import RootStore from './app/store/rootStore';
-import { QueryClient, QueryClientProvider } from 'react-query';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const container = document.getElementById('root')!;
 const root = createRoot(container);
-const client = new QueryClient();
+
 root.render(
   <React.StrictMode>
     <RootStoreContext.Provider value={new RootStore()}>
-      <QueryClientProvider client={client}>
-        <App />
-      </QueryClientProvider>
+      <App />
     </RootStoreContext.Provider>
   </React.StrictMode>,
 );
