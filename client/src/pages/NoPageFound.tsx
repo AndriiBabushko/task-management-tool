6d2fa4dbf18e71a3d3ccdf0c8c8216a3473df2a1
Andrii Babushko <andriibabushko@gmail.com>
2023-06-13 22:50:22 +0300

    client update no page found

--- client/src/pages/NoPageFound.tsx
+++ client/src/pages/NoPageFound.tsx
@@ -1,5 +1,26 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { Modal } from '../components/custom/Modal';
+import { CustomButton } from '../components/custom/CustomButton';
 
 export const NoPageFound: FC = () => {
-  return <h1>No page found</h1>;
+  const [isModalOpen, setIsModalOpen] = useState(true);
+  const navigate = useNavigate();
+
+  const openCloseModalHandler = () => {
+    setIsModalOpen(false);
+    navigate(-1);
+  };
+
+  return (
+    <Modal
+      title={'Error!'}
+      message={"Page doesn't exist! Please, go back or visit our existing pages <3"}
+      show={isModalOpen}
+      onCloseModal={openCloseModalHandler}
+    >
+      <CustomButton buttonText={'Go Back'} buttonType={'button'} onClickHandler={openCloseModalHandler} bgColor={'bg-green-900'} />
+    </Modal>
+  );
 };
