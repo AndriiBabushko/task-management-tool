798c899ed76693913b4ef9d9badba8196de96c80
Andrii Babushko <andriibabushko@gmail.com>
2023-06-13 22:48:51 +0300

    client add some task and group pages

---
+++ client/src/pages/groups/AdminGroupsPage.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from 'react';
+
+export const AdminGroupsPage: FC = () => {
+  return <h1>Admin Groups Page</h1>;
+};
---
+++ client/src/pages/tasks/UserTasksPage.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from 'react';
+
+export const UserTasksPage: FC = () => {
+  return <h1>User Tasks Page</h1>;
+};
